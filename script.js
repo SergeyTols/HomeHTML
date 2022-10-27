75257839c3e7603cf3989cdc72e1c5418a009186
Sarge <sarge2033@gmail.com>
2022-10-27 21:12:33 +0300

    new1

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert(158+2);
+
+// function sum(res) {
+//     res = 158 + 2;
+//     alert(res);
+// }
+// sum();
+
+function showMessage() {
+    let userName = prompt("Как вас зовут?","Имя");
+    let message = 'Привет, ' + userName + '! Продолжение задания №2 на второй сранице.';
+    alert(message);    
+}
+
+showMessage();
